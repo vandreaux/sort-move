91657695fc9c6d6efbcc6c0cb840fb1170de1b4b
vandreaux <vandreaux@users.noreply.github.com>
2020-03-10 21:26:34 -0400

    Add files via upload
    2020.0.10.2100 From 3D Print eSports Lab

---
+++ Untitled1.ps1
@@ -0,0 +1,14 @@
+﻿$sourcePath = 'X:\DOCO\RAW\Logos'
+
+
+Get-Childitem $sourcePath -File | Foreach-Object `
+    {
+     $target = -join($sourcePath,"\", $_.FullName.Substring($_.FullName.LastIndexOf("."), $_.FullName.Length - $_.FullName.LastIndexOf(".")).Substring(1))
+     echo $target
+     echo $_
+     mkdir $target
+     cd $sourcePath
+     $filefix = $_.fullname.replace("[", "``[").replace("]", "``]")
+     echo $filefix
+     Move-Item $filefix -Destination $target
+    }
---
+++ Untitled2.ps1
@@ -0,0 +1,31 @@
+﻿$sourcePath = 'X:\Files\OMEGA'
+// X:\Files\OMEGA\Downloads
+ 
+ 
+Get-Childitem $sourcePath -File | Foreach-Object `
+    {
+     $calculatedExt = $_.FullName.Substring($_.FullName.LastIndexOf("."), $_.FullName.Length - $_.FullName.LastIndexOf(".")).Substring(1)
+     $extPath = -join($sourcePath,"\", $calculatedExt)
+     echo -join($extPath," ",$_);
+     if (!(Test-Path -path $extPath)) { mkdir $extPath }
+     cd $extPath
+     $cleanFullName = $_.fullname.replace("[", "``[").replace("]", "``]")
+     $cleanName = $_.name.replace("[", "``[").replace("]", "``]")
+     $destination = -join($extPath,"\",$cleanName);
+     if (!(Test-Path -path $destination)) {
+        Move-Item $cleanFullName -Destination $extPath
+     } else {
+        $dup="";
+        For ($i=0; $i -le 10; $i++) {
+            $dup=$dup+"\!dup";
+            $dupPath = -join($extPath,$dup);
+            $destination = -join($dupPath,"\",$cleanName);
+            if (!(Test-Path -path $destination)) {
+                if (!(Test-Path -path $dupPath)) { mkdir $dupPath }
+                Move-Item $cleanFullName -Destination $dupPath
+                $i=10;
+            }
+        }
+
+     }
+    } 
